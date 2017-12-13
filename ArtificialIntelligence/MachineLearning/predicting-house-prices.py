502584330cd553358121a2fb58c933839fe742f7
AjayKrP <kajay5080@gmail.com>
2017-12-13 20:57:09 +0530

    house price ML

---
+++ ArtificialIntelligence/MachineLearning/predicting-house-prices.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
+a = list(map(int, input().strip().split(' ')))
+F = a[0]
+N = a[1]
+dataset = []
+
+for i in range(N):
+    arr = list(map(float, input().strip().split(' ')))
+    dataset.append(arr)
+
+dataset = np.array(dataset)
+
+X = dataset[:, :F]
+y = dataset[:, F:]
+
+
+linreg = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+linreg.fit(X_train, y_train)
+
+T = int(input())
+test_case = []
+for ij in range(T):
+    arr = list(map(float, input().strip().split(' ')))
+    test_case.append(arr)
+
+test_case = np.array(test_case)
+
+for data in test_case:
+    print(linreg.predict([data])[0][0])
