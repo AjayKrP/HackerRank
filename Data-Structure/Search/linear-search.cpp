e18d6e00144c5359670545c0123955e022119f3f
ajay <23704820+AjayKrP@users.noreply.github.com>
2018-11-09 16:45:51 +0530

    Update linear-search.cpp

--- Data-Structure/Search/linear-search.cpp
+++ Data-Structure/Search/linear-search.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <array>
+
+auto main() -> int {
+    std::array<int, 6> arr = {5, 4, 3, 8, 9, 3};
+    int key = 9;
+    bool found = false;
+    for (auto x: arr) {
+        if (x == key) {
+            found = true;
+            break;
+        }
+    }
+    if (found) {
+        std::cout << "Found!!!\n";
+    } else {
+        std::cout << "Not Found!!!\n";
+    }
+}
