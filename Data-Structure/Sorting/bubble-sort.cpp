d6df5ec1dfbc8c59b22d4f9a3c751b14fb154b5e
ajay <23704820+AjayKrP@users.noreply.github.com>
2018-11-09 17:10:10 +0530

    Create bubble-sort.cpp

---
+++ Data-Structure/Sorting/bubble-sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+auto main() -> int {
+    std::array<int, 9> arr = {1, 2, 3, 4, 5, 6, 2, 9, 0};
+    bool sorted = true;
+    for (int i = 0; i < arr.size(); ++i) {
+        for (int j = 0; j < arr.size()-1; ++j) {
+            if (arr[j] > arr[j+1]){
+                std::swap(arr[j], arr[j+1]);
+                sorted = false;
+            }
+        }
+        if (sorted) {
+            break;
+        }
+    }
+
+    //Best case complexity O(n)
+    for (auto x: arr) {
+        std::cout << x << "\t";
+    }
+}
