47e21af72aea01eebd3d7e73f4d564443eb01302
ajay <23704820+AjayKrP@users.noreply.github.com>
2018-11-10 06:56:01 +0530

    Create merge-sort.cpp

---
+++ Data-Structure/Sorting/merge-sort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+template <size_t SIZE>
+void merge(std::array<int, SIZE>&arr, int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+template <size_t SIZE>
+void mergeSort(std::array<int, SIZE>&arr, int l, int r) {
+    if (l < r) {
+        int m = l+(r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+auto main() -> int {
+    std::array<int, 9> arr = {33, 1, 200, 3, 48, 5, 6, 7, 8};
+    mergeSort(arr, 0, arr.size()-1);
+    for (auto x: arr) {
+        std::cout << x << "\t";
+    }
+}
